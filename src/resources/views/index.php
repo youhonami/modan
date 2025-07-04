2b259be189afd7a7fe8a6ba1e748fde8901151dd
youhonami <youhonami42@gmail.com>
2025-07-04 16:55:51 +0900

    ファーストコミット

---
+++ src/resources/views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPEhtml>
+    <html lang="ja">
+    <meta charset="UTF-8" />
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>COACHTECH</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sanitize.css" />
+
+
+    </head>
+
+    <body>
+        <script src="js/main.js">
+        </script>
+    </body>
+
+    </html>
